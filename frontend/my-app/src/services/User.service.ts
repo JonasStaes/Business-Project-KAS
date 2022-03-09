98bed6f71f6e7bdb0b249984b730d008515c4535
Saithamdb <77587079+Saithamdb@users.noreply.github.com>
2022-03-09 10:45:11 +0100

    Added user service

---
+++ frontend/my-app/src/services/User.service.ts
@@ -0,0 +1,17 @@
+import http from "./https-common"
+
+//make sure this is the same as the request mapping in the backend controller.
+const urlBase: string = "users"
+
+//I still need to double check how we're uploading files, so leave that out for now
+class UserService {
+    getAll() {
+        return http.get(`${urlBase}/all`)
+    }
+
+    create(name: string, email: string, active: boolean) {
+        return http.post(`${urlBase}/`, {name, email, active})
+    }
+}
+
+export default new UserService();
