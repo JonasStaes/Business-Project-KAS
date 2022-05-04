b29474a89a9d260aaa7604e15f98aee35ecbea32
Saithamdb <77587079+Saithamdb@users.noreply.github.com>
2022-05-04 13:28:23 +0200

    Update CommercialDirectionOverview
    Added button to add to whitelist, added button to go to blacklist

--- frontend/my-app/src/components/employeecomponents/commercialdirectioncomponents/CommercialDirectionOverview.component.tsx
+++ frontend/my-app/src/components/employeecomponents/commercialdirectioncomponents/CommercialDirectionOverview.component.tsx
@@ -1,5 +1,5 @@
 import { Dialog } from "@headlessui/react";
-import { ArrowCircleLeftIcon, ExclamationCircleIcon, PlusCircleIcon, TrashIcon } from "@heroicons/react/solid";
+import { ArrowCircleLeftIcon, ExclamationCircleIcon, PlusCircleIcon, TrashIcon, ArrowCircleRightIcon } from "@heroicons/react/solid";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useGetAllWhiteListEntriesQuery } from "../../../redux/features/api/commercialdirection";
@@ -19,12 +19,13 @@ export default function AdminUsers() {
       <div className="flex items-center justify-end gap-4 flex-wrap container pb-4">
         <Link to="../new_customer" className="flex items-center flex-shrink-0 text-white bg-main-0 sm:rounded-lg p-2">
           <PlusCircleIcon className="fill-current h-7 w-7 mr-2"/>
-          <span className="text-xl tracking-wider mr-2 uppercase">Klant</span>
+          <span className="text-xl tracking-wider mr-2 uppercase">Sector</span>
         </Link>
         <Link to="../new_employee" className="flex items-center flex-shrink-0 text-white bg-main-0 sm:rounded-lg p-2">
-          <PlusCircleIcon className="fill-current h-7 w-7 mr-2"/>
-          <span className="text-xl tracking-wider mr-2 uppercase">Werknemer</span>
+          <ArrowCircleRightIcon className="fill-current h-7 w-7 mr-2"/>
+          <span className="text-xl tracking-wider mr-2 uppercase">Zwarte lijst</span>
         </Link>
+        
       </div>
       <div className="bg-main-1 shadow overflow-hidden container sm:rounded-lg border-main-0 rounded border-2 h-4/5">
       {entriesLoading ? <LoadingSpinner/> : 
