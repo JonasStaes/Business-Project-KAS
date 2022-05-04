6de86c54d6b74446201b31b3ad41c0fac8999ed2
Saithamdb <77587079+Saithamdb@users.noreply.github.com>
2022-05-04 14:42:58 +0200

    Created Blacklist page + AddWhiteLIstEntry page
    - Added Blacklist overview page
    - Added Whitelistentry Create page

--- backend/kas/src/main/java/com/ap/kas/repositories/WhiteListRepository.java
+++ backend/kas/src/main/java/com/ap/kas/repositories/WhiteListRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WhiteListRepository extends JpaRepository<WhiteListEntry, String> {
 
-    boolean existsByNacebel(WhiteListEntry entry);
+    boolean existsByNacebel(String nacebel);
     
 }
