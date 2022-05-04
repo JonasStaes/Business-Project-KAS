647c6d01dc2790f47372e2c3aa0a16509269f91f
Saithamdb <77587079+Saithamdb@users.noreply.github.com>
2022-05-04 09:49:58 +0200

    Added getAll methods to CommercialManagerController
    - Added allwhitelist method
    - Added allblacklist method

--- backend/kas/src/main/java/com/ap/kas/controllers/CommercialManagerController.java
+++ backend/kas/src/main/java/com/ap/kas/controllers/CommercialManagerController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedList;
 import java.util.List;
+import com.ap.kas.repositories.WhiteListRepository;
+import com.ap.kas.repositories.BlackListRepository;
+
+import com.ap.kas.payload.response.MessageResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,8 +20,43 @@ import org.slf4j.LoggerFactory;
 @RequestMapping("commercial")
 public class CommercialManagerController {
 
+    @Autowired
+    private WhiteListRepository whiteListRepository;
+
+   @Autowired
+    private BlackListRepository blackListRepository;
+
     private static final Logger logger = LoggerFactory.getLogger(CreditRequestController.class);
 
-    
+    @GetMapping("/allwhitelist")
+    public ResponseEntity<MessageResponse> readWhiteList() {
+        try {
+            List<String> whitelistEntries = new LinkedList<String>();
+            whiteListRepository.findAll().forEach(entry -> {
+                      whitelistEntries.add(entry);
+            });
+          
+            return ResponseEntity.ok(new MessageResponse("Got all suspicious requests!", whitelistEntries));
+        } catch (Exception e) {
+            logger.error("{}", e);
+            return ResponseEntity.badRequest().body(new MessageResponse("Failed to get whitelist"));
+        }
+    }
+
+    @GetMapping("/allblacklist")
+    public ResponseEntity<MessageResponse> readBlackList(){
+        try {
+            List<String> blackListEntries = new LinkedList<String>();
+            blackListRepository.findAll().forEach(entry -> {
+                      blackListEntries.add(entry);
+            });
+          
+            return ResponseEntity.ok(new MessageResponse("Got all suspicious requests!", blackListEntries));
+        } catch (Exception e) {
+            logger.error("{}", e);
+            return ResponseEntity.badRequest().body(new MessageResponse("Failed to get whitelist"));
+        }
+
+    }
     
 }
